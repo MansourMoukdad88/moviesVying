4bae97984275a91aab5b62a3daede8b2b324c5b5
MansourMoukdad88 <m_sy@outlook.com>
2020-02-01 08:28:01 +0200

    Fetch movie data

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey:'3396a1a3',
+      s: 'dart'
+    }
+  });
+  console.log(response.data);
+}
