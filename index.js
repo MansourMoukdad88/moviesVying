5c09acc065034dfc94bc6524f12378efd21a02a7
MansourMoukdad88 <m_sy@outlook.com>
2020-02-03 04:40:45 +0200

    Avoiding Duplication of Config

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-
-createAutoComplete({
-  root: document.querySelector('.autocomplete'),
+const autoCompleteConfig = {
   renderOption(movie){
     const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
     return `
@@ -26,8 +24,16 @@ createAutoComplete({
     }
     return response.data.Search;
   }
-})
+}
 
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector('#left-autocomplete'),
+})
+createAutoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector('#right-autocomplete'),
+})
 
 const onMovieSelect = async(movie) => {
   const response = await axios.get('http://www.omdbapi.com/', {
